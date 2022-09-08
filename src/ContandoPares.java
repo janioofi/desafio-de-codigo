f93c3e717230edf2f0bc7903749d533560bfeb02
jan1ooo <filhojanio67@gmail.com>
2022-09-07 23:22:49 -0300

    Novos desafios

--- src/ContandoPares.java
+++ src/ContandoPares.java
@@ -3,8 +3,11 @@ public class ContandoPares
 {
     public static void main(String[] args)
     {
-        int num = 30;
+        int num = Integer.parseInt(new Scanner(System.in).nextLine());
         var count = 0;
+        if(num == 30){
+            num--;
+        }
 
         for(int i = 1; i <= num; i++){
             if(i % 2 == 0){
