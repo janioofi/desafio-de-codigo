1c1a7cf398daceddcabf48a0168661b35f35607e
jan1ooo <filhojanio67@gmail.com>
2023-08-12 17:51:39 -0300

    Desafio calculando custos AWS

---
+++ src/CalculandoCustoAWS.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalculandoCustoAWS {
+    public static void main(String[] args) {
+        // Definindo as taxas
+        double custoPorHoraServidor = 0.15;
+        double custoPorHoraBancoDados = 0.05;
+
+        // Criando um objeto Scanner para receber as entradas do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Recebendo as informações do usuário
+        int quantidadeServidores = scanner.nextInt();
+
+        int quantidadeBancosDados = scanner.nextInt();
+
+        //TODO: Calcule os custos
+        double custoTotal = (custoPorHoraServidor * quantidadeServidores) + (custoPorHoraBancoDados * quantidadeBancosDados);
+
+
+        System.out.printf("Custo total de uso da AWS por hora: R$ %.2f%n", custoTotal);
+
+        // Fechando o Scanner
+        scanner.close();
+    }
+}
